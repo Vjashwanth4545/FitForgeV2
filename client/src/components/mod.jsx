ce38045bff562eaed0d90c897c127356d8aca2e4
Vjashwanth4545 <jashwanth.45454@gmail.com>
2025-12-02 12:03:33 +0530

    Add vercel.json for React Router7

--- client/src/components/mod.jsx
+++ client/src/components/mod.jsx
@@ -18,7 +18,7 @@ function Modules() {
 
       <Link to="/diet" className="modbox">
         <p className="box-title">Diet Planner 
-        <FontAwesomeIcon icon={faUtensils} style={{ marginLeft: "5px" }} />
+        <FontAwesomeIcon icon={faUtensils} style={{ marginLeft: "25px" }} />
         </p>
         <p className="box-desc">Log your meals and monitor your daily caloric intake.</p>
       </Link>
